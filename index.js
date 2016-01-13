b47ed950fca1fe8f81e24aae356ce271604af36f
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-13 20:49:19 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+var request = require('request'),
+    _ = require('lodash'),
+    util = require('./util'),
+    pickInputs = {
+        campaign_id: { key: 'campaign_id', validate: { req: true } }
+    };
+
 module.exports = {
+    authOptions: function (dexter) {
+        if (!dexter.environment('mailchimp_api_key') || !dexter.environment('server')) {
+            this.fail('A [mailchimp_api_key] and [server] environment variables are required for this module');
+            return false;
+        } else {
+            return {
+                api_key: dexter.environment('mailchimp_api_key'),
+                server: dexter.environment('server')
+            }
+        }
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +24,31 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(dexter),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
+
+        if (!auth) return;
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        request({
+            method: 'POST',
+            baseUrl: 'https://' + auth.server + '.api.mailchimp.com/3.0/',
+            uri: '/campaigns/' + inputs.campaign_id + '/actions/send',
+            json: true,
+            auth: {
+                username: 'api_key',
+                password: auth.api_key
+            }
+        },
+        function (error, response, body) {
+            if (!error && (response.statusCode === 200 || response.statusCode === 204)) {
+                this.complete({});
+            } else {
+                this.fail(error || body);
+            }
+        }.bind(this));
     }
 };
